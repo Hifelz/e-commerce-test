96f51c7e088ed84bec1d71b5110757f9a175e39c
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-11 16:24:02 -0300

    Feat: Router
    Created the router and path to the different pages of the website, also changed the paste of app.css to pages/home/

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/home/Home';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Products from './pages/products/Products';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Products",
+    element: <Products />,
+  }
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </React.StrictMode>
 );
 
