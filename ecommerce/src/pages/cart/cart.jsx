6e09be2bb3e8c8fcfd2cbe8e6dea40d3560ea67a
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-17 01:01:44 -0300

    fix: unused variables and imports
    Deleted some line of the code

--- ecommerce/src/pages/cart/cart.jsx
+++ ecommerce/src/pages/cart/cart.jsx
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
-import { PRODUCTS } from "../products/Products";
+
 import { CartItem } from "./cart-item";
-import { useNavigate } from "react-router-dom";
+
 
 import "./cart.css";
 
 export const Cart = () => {
-  const navigate = useNavigate();
+
   const { cartItem } = useContext(ShopContext);
   return (
     <div className="cart-container">
@@ -21,15 +21,7 @@ export const Cart = () => {
       <div className="cart-total">
         <p>Total: ${cartItem.reduce((acc, item) => acc + item.price, 0)}</p>
       </div>
-      <div className="cart-actions">
-        <button
-          onClick={() => {
-            navigate("/checkout");
-          }}
-        >
-          Checkout
-        </button>
-      </div>
+      
       <div className="cart-total">
         <p>Total: ${cartItem.reduce((acc, item) => acc + item.price, 0)}</p>
 
