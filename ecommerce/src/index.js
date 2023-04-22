be02ff9a5b5b2d5820c4a1550b132297fb5e5f19
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-22 18:54:56 -0300

    refactor: removed page Shop and it's imports

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './pages/home/Home';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Products from './pages/products/Products';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./pages/home/Home";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Products from "./pages/products/Products";
 
-import Shop from './pages/products/product';
-
-import About from './pages/about/About';
-
-
+import About from "./pages/about/About";
 
 const router = createBrowserRouter([
   {
@@ -25,26 +18,17 @@ const router = createBrowserRouter([
     element: <Products />,
   },
   {
-    path: "/Shop",
-    element: <Shop />,
-  },
-  {
     path: "/About",
     element: <About />,
   },
-
-
 ]);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
