6423f8fb69b75887a47393f6e3de5be7ed0f7a32
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-24 23:31:52 -0300

    feat: button with link

--- ecommerce/src/pages/products/Products.js
+++ ecommerce/src/pages/products/Products.js
@@ -39,18 +39,53 @@ const Products = () => {
         <div className="container">
           <div className="box">
             <img src={img1} />
+            <p>lorem ipsum </p>
+            <h6>$400.00</h6>
+            <button>
+              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
+                BUY
+              </a>
+            </button>
           </div>
           <div className="box">
             <img src={img2} />
+            <p>lorem ipsum </p>
+            <h6>$400.00</h6>
+            <button>
+              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
+                BUY
+              </a>
+            </button>
           </div>
           <div className="box">
             <img src={img3} />
+            <p>lorem ipsum </p>
+            <h6>$400.00</h6>
+            <button>
+              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
+                BUY
+              </a>
+            </button>
           </div>
           <div className="box">
             <img src={img1} />
+            <p>lorem ipsum </p>
+            <h6>$400.00</h6>
+            <button>
+              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
+                BUY
+              </a>
+            </button>
           </div>
           <div className="box">
             <img src={img3} />
+            <p>lorem ipsum </p>
+            <h6>$400.00</h6>
+            <button>
+              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
+                BUY
+              </a>
+            </button>
           </div>
         </div>
       </header>
