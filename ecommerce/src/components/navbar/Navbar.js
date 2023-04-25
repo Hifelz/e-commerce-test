4d3909811181ddc463f6162e53374227acef615a
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-24 23:20:09 -0300

    refactor: changed css style to navbar.css

--- ecommerce/src/components/navbar/Navbar.js
+++ ecommerce/src/components/navbar/Navbar.js
@@ -1,35 +1,33 @@
 import React from "react";
 
-import "../../style/main.css";
-import {useRef} from 'react';
+import "./navbar.css";
+import { useRef } from "react";
 
 import { FaTimes, FaBars } from "react-icons/fa";
 
-
 function navbar() {
-    
-    const NavRef = useRef();
+  const NavRef = useRef();
 
-    const ShowNavBar = () => {
-        NavRef.current.classList.toggle("responsive_nav");
-    }
+  const ShowNavBar = () => {
+    NavRef.current.classList.toggle("responsive_nav");
+  };
 
-    return (
-        <header>
-            <h3>Urban Store</h3>
-            <nav ref={NavRef} />
-            <nav>
-                <a href="/">Home</a>
-                <a href="/Products">Products</a>
-                <a href="/About">About</a>
-                <button className="nav-btn nav-close-btn" onClick={ShowNavBar}>
-                    <FaTimes />
-                </button>
-                <button className="nav-btn" onClick={ShowNavBar}>
-                    <FaBars />
-                </button>
-            </nav>
-        </header>
-    );
-};
+  return (
+    <header>
+      <h3>Urban Store</h3>
+      <nav ref={NavRef} />
+      <nav>
+        <a href="/">Home</a>
+        <a href="/Products">Products</a>
+        <a href="/About">About</a>
+        <button className="nav-btn nav-close-btn" onClick={ShowNavBar}>
+          <FaTimes />
+        </button>
+        <button className="nav-btn" onClick={ShowNavBar}>
+          <FaBars />
+        </button>
+      </nav>
+    </header>
+  );
+}
 export default navbar;
