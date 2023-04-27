521e8ad8e51e956a9dab751afb4841a645eac883
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-26 23:18:28 -0300

    feat: banner of the shop

--- ecommerce/src/pages/home/Home.js
+++ ecommerce/src/pages/home/Home.js
@@ -16,70 +16,29 @@ function App() {
       <React.Fragment>
         <Navbar />
       </React.Fragment>
-
-      <div className="container">
-        <div className="gallery">
-          <img src={img1} />
-          <div className="description">
-            Iphone XR
-            <br />
-            <button>
-              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
-                BUY
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="gallery">
-          <img src={img2} />
-          <div className="description">
-            RTX 3090 TI
-            <br />
-            <button>
-              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
-                BUY
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="gallery">
-          <img src={img3} />
-          <div className="description">
-            Monitor 180Hz
-            <br />
-            <button>
-              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
-                BUY
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="gallery">
-          <img src={img2} />
-          <div className="description">
-            RTX 3090 TI
-            <br />
-            <button>
-              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
-                BUY
-              </a>
-            </button>
-          </div>
-        </div>
-        <div className="gallery">
+      <section>
+        <div className="content">
+          <h2>Loja Online </h2>
           <br />
-          <img src={img3} />
-          <div className="description">
-            Monitor 180Hz
+          <p>
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. <br></br>It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently
+            with desktop publishing software like Aldus PageMaker including
+            versions of Lorem Ipsum.
             <br />
-            <button>
-              <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
-                BUY
-              </a>
-            </button>
-          </div>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old.
+          </p>
         </div>
-      </div>
+        <div className="imageBox"></div>
+      </section>
     </>
   );
 }
