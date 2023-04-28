aaf194c82869de2f7549a3fd7a442424210aa05c
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-04-27 22:13:01 -0300

    refactor: tried to style about page

--- ecommerce/src/pages/about/About.js
+++ ecommerce/src/pages/about/About.js
@@ -1,41 +1,35 @@
+import React from "react";
 
-import React from 'react';
-
-
-import Navbar from '../../components/navbar/Navbar';
+import Navbar from "../../components/navbar/Navbar";
 import "../../style/main.css";
 
-
 const About = () => {
-    return (
+  return (
+    <div className="About">
+      <>
+        <React.Fragment>
+          <Navbar />
+        </React.Fragment>
+      </>
 
-
-        <div className="About">
-            <><React.Fragment>
-                <Navbar />
-            </React.Fragment></>
-
-                <header className="App-header">
-                    <h1>Who am I ?</h1>
-                    <br />
-                    <p>I am Hifelz a front end developer with a passion for building websites and applications. I have a passion for learning new technologies, and I love learning new things.</p>
-
-
-
-                </header>
-                <div className="rodape" align="left">
-                    <div className='teste2' align="left">
-                        <h1>
-                            Thank you for coming by
-                        </h1>
-                        <div className="teste" align="right">
-                            <h2>Contact me @Hifelz on github</h2>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-         
-            );
+      <header className="App-header">
+        <h1>Who am I ?</h1>
+        <br />
+        <p>
+          I am Hifelz a front end developer with a passion for building websites
+          and applications. I have a passion for learning new technologies, and
+          I love learning new things.
+        </p>
+      </header>
+      <div className="rodape" align="left">
+        <div className="teste2" align="left">
+          <h1>Thank you for coming by</h1>
+          <div className="teste" align="right">
+            <h2>Contact me @Hifelz on github</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-            export default About;
+export default About;
