dfae0676e367267749cbdf364d520abbf99452cf
Hifelz <100286956+Hifelz@users.noreply.github.com>
2023-05-08 22:22:31 -0300

    Update About.js

--- ecommerce/src/pages/about/About.js
+++ ecommerce/src/pages/about/About.js
@@ -1,18 +1,19 @@
 import React from "react";
-
+import axios from "axios";
+import data from "./data.json";
 import Navbar from "../../components/navbar/Navbar";
 import "../../style/main.css";
 
 const About = () => {
   axios
-      .get("https://fakestoreapiserver.reactbd.com/products")
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+    .get("https://fakestoreapiserver.reactbd.com/products")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
   return (
     <div className="About">
       <>
@@ -22,7 +23,6 @@ const About = () => {
       </>
 
       <header className="App-header">
-
         <h1>Who am I ?</h1>
         <br />
         <p>
@@ -31,25 +31,26 @@ const About = () => {
           I love learning new things.
         </p>
       </header>
+
       <div className="inner">
-          {data &&
-            data.map((data) => {
-              return (
-                <div className="box">
-                  <strong>{data.title}</strong>
-                  <br />
+        {data &&
+          data.map((data) => {
+            return (
+              <div className="box">
+                <strong>{data.title}</strong>
+                <br />
 
-                  {data.category}
-                  <button>
-                    <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
-                      BUY
-                    </a>
-                  </button>
-                </div>
-              );
-            })}
-        </div>
+                {data.category}
+                <button>
+                  <a href="https://www.amazon.com.br/PROCESSADOR-GRAFICO-MSI-GEFORCE912-V397-484-VENTUS/dp/B08WHJFYM8/ref=sr_1_3?keywords=RTX+2070&qid=1681947588&s=computers&sr=1-3&ufe=app_do%3Aamzn1.fos.25548f35-0de7-44b3-b28e-0f56f3f96147">
+                    BUY
+                  </a>
+                </button>
+              </div>
+            );
+          })}
+      </div>
     </div>
-  );
-
+  )
+}
 export default About;
